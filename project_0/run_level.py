3407588767b6ae9de2ba7d98363a3574118a686c
saqibmurtaza <saqibmurtazakhan@gmai.com>
2025-04-04 11:28:33 +0500

    Folder Structure Organized

---
+++ project_0/run_level.py
@@ -0,0 +1,45 @@
+from openai import AsyncOpenAI
+from agents import Agent, Runner, OpenAIChatCompletionsModel, function_tool, set_tracing_disabled
+from dotenv import load_dotenv
+from agents.run import RunConfig
+import os, asyncio
+
+load_dotenv()
+
+# LLM_RUN LEVEL CONFIG
+
+base_url= os.getenv("BASE_URL")
+api_key= os.getenv("API_KEY")
+model_name= os.getenv("MODEL_NAME")
+# base_url = "https://generativelanguage.googleapis.com/v1beta/openai/"
+# api_key = "AIzaSyDXZrZMgjF826B3YfwdNCN_VcnE2C45ME8"
+# model_name = "gemini-1.5-flash"
+
+print("BASE_URL:", os.getenv("BASE_URL"))
+print("API_KEY:", os.getenv("API_KEY"))
+print("MODEL_NAME:", os.getenv("MODEL_NAME"))
+
+client= AsyncOpenAI(
+    api_key=api_key,
+    base_url=base_url
+)
+
+model= OpenAIChatCompletionsModel(
+    model=model_name,
+    openai_client=client
+)
+
+config= RunConfig(
+    model=model,
+    model_provider=client,
+    tracing_disabled=True
+)
+
+agent: Agent= Agent(
+    name= "Assistant",
+    instructions="You respond in user_friendly style"
+)
+
+result= Runner.run_sync(agent, "What is the weather of Peshawar", run_config=config)
+print(result.final_output)
+
